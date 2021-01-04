065758a6e555c4754a27d8cdf177e0ddc7242f36
Naoki1218 <naoki9890@yahoo.co.jp>
2021-01-05 04:33:54 +0900

    root設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root  'users#new'
   resources :sessions, only: [:new, :create, :destroy]
   resources :favorites, only: [:create, :destroy]
   resources :photos, only: [:index,:new,:create,:edit,:update,:destroy] do
