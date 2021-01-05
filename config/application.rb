815fdb9301a7144cf173a0332e547e13ba4608d2
Naoki1218 <naoki9890@yahoo.co.jp>
2021-01-06 01:03:32 +0900

    time zone 修正

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,9 @@ module Instagram
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.time_zone = 'Tokyo'
+    config.active_record.default_timezone = :local
+    config.action_mailer.default_url_options = { host: 'glacial-reef-95295.herokuapp.com' }
     config.generators do |g|
       g.assets false
       g.helper false
